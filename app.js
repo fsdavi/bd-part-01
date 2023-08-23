b39c3b0e60ad88ea66194658fe7c9ed6e18aeb4f
Davi Freire <fsdavi.dev@gmail.com>
2023-08-23 09:09:41 -0300

    Add CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const users = [];
 
@@ -30,7 +32,7 @@ app.get('/users', (req, res) => {
  }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
